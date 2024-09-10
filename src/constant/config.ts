120806cfcad3f38c113d55236b4225bb87f268d9
maxx2002 <max.wiranata@unitcode.org>
2024-09-10 12:02:21 +0700

    feat: metadara, SEO, favicon, fonts setup

---
+++ src/constant/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: "Unit Ventures Ltd",
+  description:
+    "We empower businesses through custom mobile applications, web platforms, and strategic IT consultancy services.",
+  url: "https://www.unitventures.ltd",
+};
